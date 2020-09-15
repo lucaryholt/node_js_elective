bbfc71281cbb18acf2344a56da471d729b50fcbe
Luca Ryholt <lucaryholt@gmail.com>
2020-09-16 00:10:24 +0200

    multiple uploads

--- File_Upload_Test/app.js
+++ File_Upload_Test/app.js
@@ -2,12 +2,14 @@ const express = require("express");
 const fileUpload = require("express-fileupload"); //The library that enables file upload
 const fs = require('fs'); //file methods, used to read contents of directory
 const uuid = require("uuid"); //makes unique id, so that we get no upload conflicts
+const _ = require('lodash'); //array methods
 
 const app = express();
 
 //Here we enable file upload
 app.use(fileUpload({
-    createParentPath: true
+    createParentPath: true,
+    safeFileNames: true
 }));
 
 app.use(express.json()); //Helps to read body from request
@@ -29,7 +31,7 @@ app.get('/s/:id', (req, res) => {
                 });
             }
 
-            res.status(200).send(resBody);
+            return res.status(200).send(resBody);
         }
     }catch (error){
         console.log(error);
@@ -52,36 +54,37 @@ app.get('/download/:id/:name', (req, res) => {
     return res.status(400).send({ error: 'No such file.' });
 });
 
-//TODO upload several files to same folder
-app.post('/upload', async (req, res) => {
+app.post('/upload', async(req, res) => {
     const id = uuid.v4();
     const directory = './uploads/' + id + '/';
 
     try{
         if(!req.files){
-            res.status(400).send({
+            res.send({
                 status: false,
-                message: 'No file uploaded'
+                message: 'No files uploaded'
             });
         } else {
-            let file = req.files.file; //'file' is the name we give the input field in html / http request
+            let data = [];
 
-            file.mv(directory + file.name); //'.mv' function moves file to specified folder and file name
+            _.forEach(_.keysIn(req.files.files), (key) => {
+                let file = req.files.files[key];
 
-            res.status(201).send({
-                status: true,
-                message: 'File is uploaded',
-                data: {
-                    //id : id,
-                    //path: directory + file.name,
-                    //size: file.size,
-                    shareLink: 'localhost:8080/s/' + id
-                }
+                file.mv(directory + file.name);
+
+                data.push({
+                    name: file.name
+                });
+            });
+
+            res.status(200).send({
+                shareLink: 'localhost:8080/s/' + id,
+                files: data
             });
         }
-   } catch (error){
+    }catch (error){
         res.status(500).send(error);
-   }
+    }
 });
 
 app.listen(8080, (error) => {
