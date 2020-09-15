c584de5c8bd10ca8e22c9078084feec3292ee5a4
Luca Ryholt <lucaryholt@gmail.com>
2020-09-15 14:28:52 +0200

    get, post, put & delete handlers now working

--- 3_Car_API/app.js
+++ 3_Car_API/app.js
@@ -1,33 +1,104 @@
 const express = require("express");
 const app = express();
 
-//TODO array af JSON objekter - implementer
-//Metode til håndtere ID inkrementer
+app.use(express.json());
+
+// App variables
+// currentId is atm set to 2, as the list is hardcoded
+
+let currentId = 2;
 
 let cars = [
-    { id: 1, name: "brom brom" },
-    { id: 2, model: "Mitsubishi"}
+    { id: 1, brand: "brum" , model: "brom brom", hk: 10 },
+    { id: 2, brand: "Mitsubishi", model: "Evo", hk: 350 }
 ];
 
+// Support functions
+
+function getId(){
+    currentId++;
+    return currentId;
+}
+
+function getCar(id){
+    for(let i = 0; i < cars.length; i++){
+        if(cars[i].id === id){
+            return cars[i];
+        }
+    }
+    return null;
+}
+
+function updateCar(updatedCar, car){
+    if(updatedCar.brand !== undefined){
+        car.brand = updatedCar.brand;
+    }
+    if(updatedCar.model !== undefined){
+        car.model = updatedCar.model;
+    }
+    if(updatedCar.hk !== undefined){
+        car.hk = updatedCar.hk;
+    }
+}
+
+function correctCarFormat(car){
+    return car.brand !== undefined & car.model !== undefined & car.hk !== undefined;
+}
+
+// HTTP Requests
+
 app.get("/cars", (req, res) => {
-    return res.send(cars);
+    if(cars.length !== 0){
+        return res.send(cars).status(200);
+    }
+    return res.send({ error: 'No cars in collection.' }).status(404);
 });
 
 app.get("/cars/:id", (req, res) => {
-    return res.send({ });
+    const id = Number(req.params.id)
+    const car = getCar(id);
+    if(car != null){
+        return res.send(car).status(200);
+    }
+    return res.status(404).send({ error: 'No car with id: ' + id });
 });
 
-app.post("/cars/:id", (req, res) => {
-    return res.send({ });
+app.post("/cars", (req, res) => {
+    let newCar = req.body;
+
+    if(correctCarFormat(newCar)){
+        newCar.id = getId();
+        cars.push(newCar);
+        return res.status(200).send({ message: 'Car saved under id: ' + newCar.id });
+    }
+    return res.status(404).send({ error: 'Car did not follow correct format. Needs brand, model and hk.' });
 });
 
 app.put("/cars/:id", (req, res) => {
-    return res.send({ });
+    const id = Number(req.params.id);
+    let selectedCar = getCar(id);
+    const updatedCar = req.body;
+
+    if(selectedCar !== null){
+        updateCar(updatedCar, selectedCar);
+        return res.status(200).send({ message : 'Updated car with id: ' + id });
+    }
+
+    return res.status(404).send({ error : 'Could not find car with that id.' });
 });
 
 app.delete("/cars/:id", (req, res) => {
-    return res.send({ });
+    const id = Number(req.params.id);
+    let selectedCar = getCar(id);
+
+    if(selectedCar !== null){
+        cars.splice(cars.indexOf(selectedCar), 1);
+        return res.status(200).send({ message: 'Car deleted.', deletedCar: selectedCar });
+    }
+    return res.status(404).send({ error: 'Could not find car with id: ' + id });
 });
+
+// Listener
 
 app.listen(8080, (error) => {
    if(error){
