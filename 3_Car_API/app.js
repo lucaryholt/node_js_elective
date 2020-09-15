2846363575c2f2b59da7675bfc0895acc11d235a
Luca Ryholt <lucaryholt@gmail.com>
2020-09-15 14:36:45 +0200

    small refactor

--- 3_Car_API/app.js
+++ 3_Car_API/app.js
@@ -29,6 +29,11 @@ function getCar(id){
     return null;
 }
 
+function addCar(car){
+    car.id = getId();
+    cars.push(car);
+}
+
 function updateCar(updatedCar, car){
     if(updatedCar.brand !== undefined){
         car.brand = updatedCar.brand;
@@ -49,7 +54,7 @@ function correctCarFormat(car){
 
 app.get("/cars", (req, res) => {
     if(cars.length !== 0){
-        return res.send(cars).status(200);
+        return res.status(200).send(cars);
     }
     return res.status(404).send({ error: 'No cars in collection.' });
 });
@@ -58,7 +63,7 @@ app.get("/cars/:id", (req, res) => {
     const id = Number(req.params.id)
     const car = getCar(id);
     if(car != null){
-        return res.send(car).status(200);
+        return res.status(200).send(car);
     }
     return res.status(404).send({ error: 'No car with id: ' + id });
 });
@@ -67,8 +72,7 @@ app.post("/cars", (req, res) => {
     let newCar = req.body;
 
     if(correctCarFormat(newCar)){
-        newCar.id = getId();
-        cars.push(newCar);
+        addCar(newCar);
         return res.status(201).send({ message: 'Car saved under id: ' + newCar.id });
     }
     return res.status(406).send({ error: 'Car did not follow correct format. Needs brand, model and hk.' });
